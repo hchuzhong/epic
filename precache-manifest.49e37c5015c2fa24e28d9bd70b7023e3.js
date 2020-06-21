47f469438f2545ea9e0105fd873d5a0c48f160f8
chuzhong <352995096@qq.com>
2020-06-21 19:28:20 +0800

    Updates

---
+++ precache-manifest.49e37c5015c2fa24e28d9bd70b7023e3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53866fb69ba8dd158e742c6c5181f283",
+    "url": "./epic/index.html"
+  },
+  {
+    "revision": "07435e235b9f2196fb4e",
+    "url": "./epic/static/css/5.f47422e6.chunk.css"
+  },
+  {
+    "revision": "030944da76068fe6b9fe",
+    "url": "./epic/static/css/main.543e4e90.chunk.css"
+  },
+  {
+    "revision": "38a519de4a93b68565b2",
+    "url": "./epic/static/js/0.1da7b164.chunk.js"
+  },
+  {
+    "revision": "35ef87f2edfeaf56a05e",
+    "url": "./epic/static/js/1.0433c338.chunk.js"
+  },
+  {
+    "revision": "5fffe85de62e9c862866",
+    "url": "./epic/static/js/10.25b5defe.chunk.js"
+  },
+  {
+    "revision": "e68322083d16eeb5c079",
+    "url": "./epic/static/js/11.0271f99b.chunk.js"
+  },
+  {
+    "revision": "50f7ede0105afb837414",
+    "url": "./epic/static/js/12.fcdb0e3e.chunk.js"
+  },
+  {
+    "revision": "378e049e7870c1fcb22c",
+    "url": "./epic/static/js/2.95a3bf70.chunk.js"
+  },
+  {
+    "revision": "07435e235b9f2196fb4e",
+    "url": "./epic/static/js/5.271600c8.chunk.js"
+  },
+  {
+    "revision": "6c48000cb7e3fe060b671159dcdfb038",
+    "url": "./epic/static/js/5.271600c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b832151614bc6dc907d",
+    "url": "./epic/static/js/6.7d663d91.chunk.js"
+  },
+  {
+    "revision": "0af14472359369717d47",
+    "url": "./epic/static/js/7.2d4b1940.chunk.js"
+  },
+  {
+    "revision": "641e0a0c8b395811a0a2",
+    "url": "./epic/static/js/8.ece0b0d7.chunk.js"
+  },
+  {
+    "revision": "4d29067806957f210470",
+    "url": "./epic/static/js/9.cb49bca6.chunk.js"
+  },
+  {
+    "revision": "030944da76068fe6b9fe",
+    "url": "./epic/static/js/main.8b36642b.chunk.js"
+  },
+  {
+    "revision": "2920c585c3d424d0be4c",
+    "url": "./epic/static/js/runtime-main.7a63f4b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./epic/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.03133b880506b8776703594709a0b582.js"
+  "./epic/precache-manifest.49e37c5015c2fa24e28d9bd70b7023e3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./epic/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
