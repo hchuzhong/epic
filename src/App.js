d889dc8afcf3b3312f0b97947d30585e6edae515
chuzhong <352995096@qq.com>
2020-06-24 13:21:07 +0800

    å¢update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Suspense, lazy} from 'react';
-import './App.css';
+import React, {Suspense, lazy} from 'react'
+import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Loading from './components/Loading'
 
-import {Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 const Home = lazy(() => import('./pages/Home'))
 const History = lazy(() => import('./pages/History'))
@@ -19,18 +19,21 @@ function App() {
       <Header/>
       <main>
         <Suspense fallback={<Loading/>}>
-          <Switch>
-            <Route path="/" exact component={Home}/>
-            <Route path="/history" component={History}/>
-            <Route path="/about" component={About}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/register" component={Register}/>
-          </Switch>
+          <Router>
+            <Switch>
+              <Route path="/" exact component={Home}/>
+              <Route path="/history" component={History}/>
+              <Route path="/about" component={About}/>
+              <Route path="/login" component={Login}/>
+              <Route path="/register" component={Register}/>
+            </Switch>
+          </Router>
+
         </Suspense>
       </main>
       <Footer/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
